faf2864e3b321fa1ee748a6c0797d00c97b09d78
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2023-02-27 18:56:10 +0200

    Rework into single function

--- lmserver/view.py
+++ lmserver/view.py
@@ -16,14 +16,33 @@ def root():
 
 
 @bp.route('/')
-def show_index():
-    return render_template('index.html')
-
-
 @bp.route('/generate', methods=['GET', 'POST'])
-def tag_text():
-    text = str(request.values['text'])
-    format_ = str(request.values.get('format', 'html'))
-    generation = lmserver.model.generate(app.tokenizer, app.model, text)
-    print(generation)
+def show_index():
+    try:
+        prompt = str(request.values['text'])
+        prompt = prompt.replace('\r\n', '\n')
+    except:
+        prompt = ""
+    try:
+        length = int(request.values['length'])
+    except:
+        length = 10
+    try:
+        temperature = float(request.values['temperature'])
+    except:
+        temperature =  0.7
+    if not prompt:
+        generation = ""
+    else:
+        generation = lmserver.model.generate(
+            app.tokenizer,
+            app.model,
+            prompt,
+            temperature=temperature,
+            new_tokens=length,
+        )
+        if generation.startswith(prompt):
+            generation = generation[len(prompt):]
+        else:
+            print(f'MISMATCH:\n"{repr(prompt)}"\n"{repr(generation)}"')
     return render_template('index.html', **locals())
