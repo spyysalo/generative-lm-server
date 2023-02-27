cdfc4a862e751d7c5e578f00bd90e2bcbc5b3aea
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2023-02-27 18:58:41 +0200

    Fixed number of generated tokens

--- lmserver/model.py
+++ lmserver/model.py
@@ -7,13 +7,14 @@ def load(name):
     return tokenizer, model
 
 
-def generate(tokenizer, model, prompt, temperature=0.7, max_new_tokens=50):
+def generate(tokenizer, model, prompt, temperature=0.7, new_tokens=50):
     input = tokenizer(prompt, return_tensors='pt')
     output = model.generate(
         **input,
         do_sample=True,
         temperature=temperature,
-        max_new_tokens=max_new_tokens,
+        min_new_tokens=new_tokens,
+        max_new_tokens=new_tokens,
         no_repeat_ngram_size=2,
     )
     return tokenizer.decode(output[0], skip_special_tokens=True)
