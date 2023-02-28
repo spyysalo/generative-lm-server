b0c814fde8a65291e9b9386023f5172e3d15c529
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2023-02-28 16:06:34 +0200

    Restructure with class

--- lmserver/model.py
+++ lmserver/model.py
@@ -1,20 +1,32 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-def load(name):
-    tokenizer = AutoTokenizer.from_pretrained(name)
-    model = AutoModelForCausalLM.from_pretrained(name)
-    return tokenizer, model
+class LocalModel:
+    def __init__(self, tokenizer, model):
+        self.tokenizer = tokenizer
+        self.model = model
+
+    def generate(self, prompt, temperature=0.7, new_tokens=50):
+        input_ = self.tokenizer(prompt, return_tensors='pt')
+        output = self.model.generate(
+            **input_,
+            do_sample=True,
+            temperature=temperature,
+            min_new_tokens=new_tokens,
+            max_new_tokens=new_tokens,
+            no_repeat_ngram_size=2,
+        )
+        return self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+    @classmethod
+    def load(cls, name):
+        tokenizer = AutoTokenizer.from_pretrained(name)
+        model = AutoModelForCausalLM.from_pretrained(name)
+        return cls(tokenizer, model)
 
 
-def generate(tokenizer, model, prompt, temperature=0.7, new_tokens=50):
-    input = tokenizer(prompt, return_tensors='pt')
-    output = model.generate(
-        **input,
-        do_sample=True,
-        temperature=temperature,
-        min_new_tokens=new_tokens,
-        max_new_tokens=new_tokens,
-        no_repeat_ngram_size=2,
-    )
-    return tokenizer.decode(output[0], skip_special_tokens=True)
+def setup(name):
+    if name.startswith('http'):
+        raise NotImplementedError
+    else:
+        return LocalModel.load(name)
